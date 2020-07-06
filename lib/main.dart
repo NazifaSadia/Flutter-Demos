f1b324432fc95c4dafbd8285524eef0627afbaf7
NazifaSadia <nazifas323@gmail.com>
2020-07-06 11:12:49 +0600

    Hello World!

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,13 @@
 import "package:flutter/material.dart";
-import './app_screens/first_screen.dart';
 
-void main() => runApp(new MyFlutterApp());
+void main() {
 
-class MyFlutterApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "My Flutter App",
-        home: Scaffold(
-            appBar: AppBar(title: Text("My First App Screen"),),
-            body: FirstScreen()
-        )
-    );
-  }
+  runApp(
+      Center(
+        child: Text(
+            "Hello Flutter",
+            textDirection: TextDirection.ltr
+        ),
+      )
+  );
 }
